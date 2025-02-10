8dfb652f95384bfbf6898e383a4316c3e99613e1
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-02-10 17:30:44 +0200

    Update script.js

--- assets/js/contact/script.js
+++ assets/js/contact/script.js
@@ -2,11 +2,3 @@ document.getElementById('back-button').addEventListener('click', function() {
       window.history.back();
     });
 
-document.getElementById('nav-button').addEventListener('click', function() {
-    var url = 'https://www.google.com/maps/dir/?api=1&destination=-26.011694,28.123784';
-    if (navigator.userAgent.match(/Mobile|Android|iPhone/i)) {
-        window.open(url, '_self');
-    } else {
-        window.open(url, '_blank');
-    }
-});
