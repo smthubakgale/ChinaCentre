40d3891150cd731ea387d4bc770b047abfed93e3
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-02-14 15:54:34 +0200

    Update script.js

--- assets/js/products/script.js
+++ assets/js/products/script.js
@@ -2,6 +2,11 @@ const filterBtn = document.querySelector('.filter-btn');
 const filterAside = document.querySelector('.filter-aside');
 const closeButton = document.querySelector('.filter-close');
 
+const sortContainer = document.querySelector('.sort-container');
+const sortOptions = document.querySelector('.sort-options');
+const chevronIcon = document.querySelector('.chevron-icon i');
+
+// Pop Up 
 filterBtn.addEventListener('click', () => {
   filterAside.classList.toggle("pop-show");
 });
@@ -9,6 +14,19 @@ filterBtn.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   filterAside.classList.remove("pop-show");
 });
+
+// Aside Sort 
+sortContainer.addEventListener('click', () => {
+  if (sortOptions.style.display === 'none') {
+    sortOptions.style.display = 'block';
+    chevronIcon.className = 'fas fa-chevron-up';
+  } else {
+    sortOptions.style.display = 'none';
+    chevronIcon.className = 'fas fa-chevron-down';
+  }
+});
+
+//
 
 
 
