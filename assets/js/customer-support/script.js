b733f157bae3945417c02834fe9be9900f2a3449
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-02-26 14:38:00 +0200

    Update script.js

--- assets/js/customer-support/script.js
+++ assets/js/customer-support/script.js
@@ -39,20 +39,12 @@ function updateWaveform() {
 var voiceCallModal = document.getElementById("voice-call-modal");
 var voiceCallButton = document.querySelector(".chat-header-button");
 
-// Get the close button
-var closeButton = document.querySelector(".close");
-
 // Get the end call button
 var endCallButton = document.getElementById("end-call-button");
 
 // Open the modal when the voice call button is clicked
 voiceCallButton.addEventListener("click", function() {
   voiceCallModal.style.display = "block";
-});
-
-// Close the modal when the close button is clicked
-closeButton.addEventListener("click", function() {
-  voiceCallModal.style.display = "none";
 });
 
 // Close the modal when the end call button is clicked
