4c2d4b78a2a268b33d2636734ccfd08944cc93e5
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-02-27 16:07:24 +0200

    Update script.js

--- assets/js/tax-invoice/script.js
+++ assets/js/tax-invoice/script.js
@@ -1,4 +1,50 @@
+// Get the download invoice button
+const downloadInvoiceButton = document.getElementById('print-button');
 
+// Add an event listener to the download invoice button
+downloadInvoiceButton.addEventListener('click', () => {
+    // Get the HTML content of the invoice page
+    const element = document.querySelector('.preview-tab');
+    const clonedElement = element.cloneNode(true);
+    const actionsElements = clonedElement.querySelectorAll('.actions');
+
+    actionsElements.forEach((action)=>
+    {
+        action.remove();
+    });
+    
+    const htmlContent = clonedElement.outerHTML;
+    // Get the CSS styles from style tags
+    const styleTags = Array.from(document.querySelectorAll('style'));
+    const styleTagCss = styleTags.map(tag => tag.innerHTML).join('');
+
+    // Get the inline CSS styles
+    const inlineCss = Array.from(document.querySelectorAll('[style]')).map(element => {
+        const selector = element.tagName.toLowerCase();
+        return `${selector} { ${element.getAttribute('style')} }`;
+    }).join('');
+
+    // Create a new blob with the HTML content, CSS styles, and inline CSS styles
+    const blob = new Blob([`
+        <style>
+            ${styleTagCss}
+            ${inlineCss}
+        </style>
+        ${htmlContent}
+    `], { type: 'text/html' });
+
+    // Create a new link element
+    const link = document.createElement('a');
+
+    // Set the link's href attribute to the blob
+    link.href = URL.createObjectURL(blob);
+
+    // Set the link's download attribute to the file name
+    link.download = 'invoice.html';
+
+    // Simulate a click on the link
+    link.click();
+});
 // Tabs 
 function openTab(evt, cityName) {
     var i, tabcontent, tablinks;
