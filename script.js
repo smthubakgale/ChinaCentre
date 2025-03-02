5715510164553f70e386d00c39b908cd246c3722
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-02 09:14:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Modals 
+const configIcon = document.querySelector('.config-icon');
+const modal = document.querySelector('.modal');
+
+configIcon.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
 // Get elements 
 const checkWrapper = document.querySelectorAll('.check-wrapper');
 
