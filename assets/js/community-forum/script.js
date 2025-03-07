d463c8932fba059c3b27859c13ddf1c643455b60
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-07 10:59:53 +0200

    Update script.js

--- assets/js/community-forum/script.js
+++ assets/js/community-forum/script.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById("new-thread-modal");
+var modal2 = document.getElementById("new-thread-modal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("new-thread-btn");
@@ -9,18 +9,18 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
 
