81460fd14cc9e9b304a078059d9863c8b6d4b298
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-18 16:06:56 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,2 +1,8 @@
+const navLinks = document.querySelectorAll('.nav-link');
 
+const hrefs = Array.from(navLinks).map(link => link.getAttribute('href').replace('#', ''));
+
+const jsonData = JSON.stringify(hrefs);
+
+console.log(jsonData);
 
