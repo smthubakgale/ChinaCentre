26bbfe053f815824d842e82315aae811c0171d17
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-19 17:17:09 +0200

    Update script.js

--- assets/js/more/script.js
+++ assets/js/more/script.js
@@ -1,1 +1,13 @@
 
+const url = api_url + `database/tables?session=${encodeURIComponent(session)}`;
+console.log(url);
+    
+fetch(url)
+.then((response) => response.json())
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+
+});
+//:
