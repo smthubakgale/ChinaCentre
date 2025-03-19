f20bfa4284177d8fc5dbac7beaad68f58b2e033b
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-19 09:43:38 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,138 @@
 
+const user_management = [
+  { 
+    "page": "home", 
+    "users": [] 
+  },
+  { 
+    "page": "home", 
+    "users": [] 
+  },
+  { 
+    "page": "collections", 
+    "users": [] 
+  },
+  { 
+    "page": "notifications", 
+    "users": [] 
+  },
+  { 
+    "page": "register", 
+    "users": [] 
+  },
+  { 
+    "page": "profile-settings", 
+    "users": [] 
+  },
+  { 
+    "page": "logout", 
+    "users": [] 
+  },
+  { 
+    "page": "dashboard", 
+    "users": [] 
+  },
+  { 
+    "page": "wishlist", 
+    "users": [] 
+  },
+  { 
+    "page": "order-history", 
+    "users": [] 
+  },
+  { 
+    "page": "payment-history", 
+    "users": [] 
+  },
+  { 
+    "page": "address-book", 
+    "users": [] 
+  },
+  { 
+    "page": "knowledge-base", 
+    "users": [] 
+  },
+  { 
+    "page": "faqs", 
+    "users": [] 
+  },
+  { 
+    "page": "glossary", 
+    "users": [] 
+  },
+  { 
+    "page": "sitemap", 
+    "users": [] 
+  },
+  { 
+    "page": "terms-of-use", 
+    "users": [] 
+  },
+  { 
+    "page": "disclaimer", 
+    "users": [] 
+  },
+  { 
+    "page": "privacy-policy", 
+    "users": [] 
+  },
+  { 
+    "page": "shipping-policy", 
+    "users": [] 
+  },
+  { 
+    "page": "payment-methods", 
+    "users": [] 
+  },
+  { 
+    "page": "about", 
+    "users": [] 
+  },
+  { 
+    "page": "blogs", 
+    "users": [] 
+  },
+  { 
+    "page": "events-calendar", 
+    "users": [] 
+  },
+  { 
+    "page": "affiliate-program", 
+    "users": [] 
+  },
+  { 
+    "page": "careers", 
+    "users": [] 
+  },
+  { 
+    "page": "loyalty-program", 
+    "users": [] 
+  },
+  { 
+    "page": "community-forum", 
+    "users": [] 
+  },
+  { 
+    "page": "contact", 
+    "users": [] 
+  },
+  { 
+    "page": "customer-support", 
+    "users": [] 
+  },
+  { 
+    "page": "tax-invoice", 
+    "users": ["Manager"] 
+  },
+  { 
+    "page": "cart", 
+    "users": [] 
+  },
+  { 
+    "page": "checkout", 
+    "users": [] 
+  }
+];
+
+
+
