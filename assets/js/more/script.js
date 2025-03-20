6aa4167f809204f466762dd6f28d40006eae5658
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-20 02:59:35 +0200

    Update script.js

--- assets/js/more/script.js
+++ assets/js/more/script.js
@@ -6,6 +6,25 @@ fetch(url)
 .then((response) => response.json())
 .then((data) => {
     console.log(data);
+
+    if (data.success && data.tables.length > 0) {
+      const databaseList = document.querySelector('#database-list');
+      const paragraph = databaseList.querySelector('p');
+      const unorderedList = databaseList.querySelector('ul');
+    
+      paragraph.textContent = "Databases";
+    
+      data.tables.forEach((table) => {
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = "#database";
+        link.dataset.queries = `table=${table}`; // Use dataset for custom attributes
+        link.classList.add("nav-link");
+        link.innerHTML = `${table} <i class="fas fa-chevron-right"></i>`;
+        listItem.appendChild(link);
+        unorderedList.appendChild(listItem);
+      });
+    }
 })
 .catch((error) => {
 
