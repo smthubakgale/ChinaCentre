c15f3c85cbe10cc7ada7cf2dafaab956c466f0ca
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-21 15:34:40 +0200

    Update script.js

--- assets/js/logout/script.js
+++ assets/js/logout/script.js
@@ -1,17 +1,9 @@
-var logout = { 
-  init: () => {
-   // Get the login again button
-    const loginAgainButton = document.getElementById('login-again-btn');
-    
-    // Add event listener to the login again button
-    loginAgainButton.addEventListener('click', () => {
-        // Redirect to the login page
-        window.location.href = 'login.html';
-    });    
-  }
-};
 
-// Initialize the page
-logout.init();
+window.logout();
 
+setTimeout(()=>
+{
+  loadPage('register');
+  renderFill('screen'); 
+},2000);
     
