ab82fe4c8a2b1cdfb6395cfe2ffa02e96996bac6
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-22 09:58:39 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -95,7 +95,7 @@ fetch(url)
             let query = `INSERT INTO ${param.table} (${columns.join(', ')}) VALUES (${values.join(', ')})`;
  
             // Send the form data to the server using fetch API
-            fetch(d_config.url + `/database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query)}`)
+            fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query)}`)
             .then((response) => { 
                 return response.text()
             })
