13fa6cfb1ae9ef842f7a0cb5355a5879249532b9
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-22 09:22:26 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -1,12 +1,7 @@
-
-console.log(window.queryParam); 
-
-//: 
 let param = window.queryParam;
 
 const url = d_config.url + `database/table?session=${encodeURIComponent(session)}&table=${param.table}`;
-console.log(url);
-    
+
 fetch(url)
 .then((response) => response.json())
 .then((data) => {
