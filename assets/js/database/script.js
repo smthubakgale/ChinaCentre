f1b9e942629c5e342ee64f31d20c82fa683a8213
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-23 09:36:05 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -178,10 +178,6 @@ fetch(url2)
         });
         
         // Add a button to open the modal
-        let addButtonHtml = `
-            <button type="button" class="btn btn-primary" data-toggle="modal"  data-backdrop="false" data-target="#add-item-modal">Add New Item</button>
-        `;
-        document.getElementById('filter-container').innerHTML += addButtonHtml;
         // Append the table and filters to the page
         document.getElementById("banner-container").innerHTML = bannerHtml;
         document.getElementById("filter-container").innerHTML += filtersHtml;
