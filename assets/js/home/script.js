26700a5860326a120c84989fe6f50f1b0065f2e8
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-03-31 15:49:03 +0200

    Update script.js

--- assets/js/home/script.js
+++ assets/js/home/script.js
@@ -61,3 +61,64 @@ fetch(d_config.url + `database/query/exec?session=${encodeURIComponent(session)}
     console.error(error);
 });
 
+// 2. Read Brands 
+
+query =  `SELECT TOP 6 *
+FROM Brands
+ORDER BY NEWID();`;
+
+fetch(d_config.url + `database/query/exec?session=${encodeURIComponent(session)}&query=${btoa(query)}`)
+.then((response) => response.json())
+.then((data) => { 
+     console.log(data);
+    if(data.success && data.results)
+    {
+         data.results.recordset.forEach((item)=>
+         {
+              console.log(item);
+              
+             let brand = document.querySelector('.explore-brands .item').cloneNode(true);
+             const h5 = brand.querySelector("h5");
+             const img = brand.querySelector("img");
+              
+             h5.innerHTML = item.brand_name;
+              
+              fetch(d_config.url + `list-files?session='${encodeURIComponent(session)}'&tableName=Brands&tableIdx=${item.idx}`)
+             .then(response => response.json())
+             .then((data) => 
+              {   
+          	 var proc = true; 
+          	 if(data.recordset)
+          	 {
+          	   console.log(data.recordset);
+          	   data.recordset.forEach((item)=>
+          	   {  
+          			if(item.file_name && item.file_size && item.gallery == "NO" && proc)
+          			{
+          			   proc = false ;
+          			   
+          			   img.src = `${d_config.url}get-file?session='${encodeURIComponent(session)}'&tableName=Brands&idx=${encodeURI(item.idx)}`;
+          			}				   
+          		});
+          	 }
+          	   
+          	 if(proc){
+          	   const icon = document.createElement("i");
+          		icon.className = "fas fa-image";
+          		icon.title = "No image available";
+          		img.insertAdjacentElement("afterend", icon);
+          		img.style.display = "none";
+           
+          	 }
+             })
+             .catch(error => console.error('Error:', error));
+              
+             img.alt = item.brand_name;
+
+             document.querySelector('.explore-brands .final').appendChild(brand);
+         });
+    }
+})
+.catch((error) => {
+    console.error(error);
+});
