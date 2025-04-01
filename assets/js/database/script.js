11c277537d5c86d1fb6cf1ac922e86d5134271d1
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-01 15:05:37 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -203,7 +203,7 @@ setTimeout(function()
 			  
 			   formFieldsHtml += `
 			    <div class="form-group col-md-3">
-			      <label for="${column.name}">${filterName}</label>
+			      <label for="${column.name}">${fieldName}</label>
 			      <input type="${column.form}" class="form-control" id="${column.name}" placeholder="${fieldName}" ${inputAttributes}>
 			    </div>
 			  `;
