36c7957feb17efa2a4b48cab96d96db0f87664ee
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-01 02:07:05 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -236,8 +236,7 @@ setTimeout(function()
 		    }
 
 		    console.log(query);
-			
-		    return;
+		
 	            // Send the form data to the server using fetch API
 	            fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query)}`)
 	            .then((response) => { 
