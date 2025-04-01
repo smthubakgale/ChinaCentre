715c93621130b25d5f1df1a1ae3a94781b2ad180
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-01 14:54:54 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -161,11 +161,32 @@ setTimeout(function()
 			  formFieldsHtml += `
 			  <div class="form-group">
 			    <label for="${column.name}">${fieldName}</label>
-			    <input type="range" class="form-control-range" id="${column.name}-min" name="${column.name}-min" min="0" max="100" value="0" 
-			           oninput="this.parentNode.querySelector('span').textContent = this.value + ' - ' + this.parentNode.querySelector('#${column.name}-max').value;">
-			    <input type="range" class="form-control-range" id="${column.name}-max" name="${column.name}-max" min="0" max="100" value="100" 
-			           oninput="this.parentNode.querySelector('span').textContent = this.parentNode.querySelector('#${column.name}-min').value + ' - ' + this.value;">
-			    <span id="${column.name}-value">0 - 100</span>
+			    <div class="range">
+				  <div class="range-slider">
+				    <span class="range-selected"></span>
+				  </div>
+				  <div class="range-input">
+				    <input type="range" class="min form-control-range" id="${column.name}-min" name="${column.name}-min" min="0" max="100" value="0" 
+				           oninput="let minVal = parseInt(this.value); 
+				                     let maxVal = parseInt(this.parentNode.querySelector('#${column.name}-max').value); 
+				                     if (minVal >= maxVal) { 
+				                       this.value = maxVal - 1; 
+				                       minVal = maxVal - 1; 
+				                     } 
+				                     this.parentNode.parentNode.querySelector('.range-selected').style.left = (minVal / 100) * 100 + '%'; 
+				                     this.parentNode.parentNode.querySelector('#${column.name}-value').textContent = minVal + ' - ' + maxVal;">
+				    <input type="range" class="max form-control-range" id="${column.name}-max" name="${column.name}-max" min="0" max="100" value="100" 
+				           oninput="let maxVal = parseInt(this.value); 
+				                     let minVal = parseInt(this.parentNode.querySelector('#${column.name}-min').value); 
+				                     if (maxVal <= minVal) { 
+				                       this.value = minVal + 1; 
+				                       maxVal = minVal + 1; 
+				                     } 
+				                     this.parentNode.parentNode.querySelector('.range-selected').style.right = 100 - (maxVal / 100) * 100 + '%'; 
+				                     this.parentNode.parentNode.querySelector('#${column.name}-value').textContent = minVal + ' - ' + maxVal;">
+				  </div>
+				  <span id="${column.name}-value" class="range-value" >0 - 100</span>
+				</div>
 			  </div>
 			`;
 			} 
