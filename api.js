0ed4ebd1356684f6a9958efd607485adfa766c21
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-05 09:36:00 +0200

    Update api.js

--- api.js
+++ api.js
@@ -61,11 +61,11 @@ function flashMessage(message, type = 'success') {
     messageElement.style.transition = 'opacity 0.5s';
     
     setTimeout(() => {
-      messageElement.style.opacity = 0;
+      //messageElement.style.opacity = 0;
       
       setTimeout(() => {
-        messageElement.remove();
-        flashMessageContainer.remove();
+        //messageElement.remove();
+        //flashMessageContainer.remove();
       }, 500);
     }, 2000);
   }, 0);
