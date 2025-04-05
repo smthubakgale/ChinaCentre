3e2a0818f9611535b0eae64fd4defd3687e0085d
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-05 15:29:39 +0200

    Update api.js

--- api.js
+++ api.js
@@ -52,19 +52,7 @@ function loadCart() {
                     <div class="product-actions">
                       <div class="quantity-selector">
                         <span class="qty-label">Qty</span>
-                        <input type="number" list="quantities" value="${item.quantity}">
-                        <datalist id="quantities">
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                          <option value="6">6</option>
-                          <option value="7">7</option>
-                          <option value="8">8</option>
-                          <option value="9">9</option>
-                          <option value="10">10</option> 
-                        </datalist>
+                        <input type="number" style="border:none;" value="${item.quantity} min="1"> 
                       </div>
                       <div class="delete-icon">
                         <i class="fas fa-trash-alt"></i>
