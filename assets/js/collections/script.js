38d8d50a1d60e73c441678697d599cc9e8030183
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-07 17:26:51 +0200

    Update script.js

--- assets/js/collections/script.js
+++ assets/js/collections/script.js
@@ -1,3 +1,69 @@
+// Read Departments
+let query =  `SELECT TOP 6 *
+FROM Departments
+ORDER BY NEWID();`;
+
+fetch(d_config.url + `database/query/exec?session=${encodeURIComponent(session)}&query=${btoa(query)}`)
+.then((response) => response.json())
+.then((data) => { 
+     console.log(data);
+    if(data.success && data.results)
+    {
+         data.results.recordset.forEach((item)=>
+         {
+              console.log(item);
+              
+             let department = new DOMParser().parseFromString(
+              `<div class="department nav-link" href="#departments" queries="${'department=' + item.idx}" >
+                 <img src="" alt="">
+                 <div></div>
+               </div> `, 
+              "text/html").body.firstChild;
+              
+             const dv = department.querySelector("div");
+             const img = department.querySelector("img");
+              
+             dv.innerHTML = item.department_name;
+              
+              fetch(d_config.url + `list-files?session='${encodeURIComponent(session)}'&tableName=Departments&tableIdx=${item.idx}`)
+             .then(response => response.json())
+             .then((data) => 
+              {   
+                var proc = true; 
+                if(data.recordset)
+                {
+                  console.log(data.recordset);
+                  data.recordset.forEach((item)=>
+                  {  
+                         if(item.file_name && item.file_size && item.gallery == "NO" && proc)
+                         {
+                            proc = false ;
+                            
+                            img.src = `${d_config.url}get-file?session='${encodeURIComponent(session)}'&tableName=Departments&idx=${encodeURI(item.idx)}`;
+                         }				   
+                    });
+                }
+                  
+                if(proc){
+                  const icon = document.createElement("i");
+                    icon.className = "fas fa-image";
+                    icon.title = "No image available";
+                    img.insertAdjacentElement("afterend", icon);
+                    img.style.display = "none";
+           
+                }
+             })
+             .catch(error => console.error('Error:', error));
+              
+             img.alt = item.department_name;
+
+             document.querySelector('.departments-grid.final').appendChild(department);
+         });
+    }
+})
+.catch((error) => {
+    console.error(error);
+});
 // Article Section
 
 const readMoreButton = document.querySelector('.read-more');
