bef017f60b585d0afb2986da4f9553c7fb9f6aea
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-09 11:00:45 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -1327,28 +1327,9 @@ setTimeout(function()
 			       console.error(err);
 		           });
 		    }
-		    else if(column.form == "editor"){
-			 // Initialize CKEditor 
-                         CKEDITOR.replace(`${column.name}` , {
-			    height: 200,
-			    toolbar: [
-			      { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord' ] },
-			      { name: 'editing', items: [ 'Find', 'Replace', 'SelectAll' ] },
-			      { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
-			      '/',
-			      { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'RemoveFormat' ] },
-			      { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'BidiLtr', 'BidiRtl' ] },
-			      { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-			      { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak' ] },
-			      '/',
-			      { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-			      { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-			      { name: 'tools', items: [ 'Maximize', 'ShowBlocks', 'About' ] }
-			    ],
-			    disableSecurityWarning: true
-			  });
-			 // 
-			 CKEDITOR.instances[${column.name}].setData('Your content here');
+		    else if(column.form == "editor"){ 
+                         CKEDITOR.replace(`${column.name}`); 
+			 CKEDITOR.instances[${column.name}].setData('');
 		    }
 		    else if(column.form == "select")
 		    { 
