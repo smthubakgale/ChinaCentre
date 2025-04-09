62407971d5a6f17b621f97f6da9ad48bd52967e8
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-09 17:44:06 +0200

    Update script.js

--- assets/js/departments/script.js
+++ assets/js/departments/script.js
@@ -15,6 +15,70 @@ console.log(Params , did);
 
 if(did)
 {
+// Read Categorys
+let query =  `SELECT TOP 16 *
+FROM Categories
+WHERE idx = ${did}
+ORDER BY NEWID();`;
 
+fetch(d_config.url + `database/query/exec?session=${encodeURIComponent(session)}&query=${btoa(query)}`)
+.then((response) => response.json())
+.then((data) => { 
+     //console.log(data);
+    if(data.success && data.results)
+    {
+         data.results.recordset.forEach((item)=>
+         {
+              //console.log(item);
+              
+             let category = new DOMParser().parseFromString( `
+                  <div href="#products" class="category nav-link"  queries="${'category=' + item.idx}"  >
+                     <img src="" alt="">
+                     <div>${item.category_name}</div>
+                  </div>
+              `, 
+              "text/html").body.firstChild; 
+            
+              const img = category.querySelector("img");
+                
+              fetch(d_config.url + `list-files?session='${encodeURIComponent(session)}'&tableName=Categories&tableIdx=${item.idx}`)
+             .then(response => response.json())
+             .then((data) => 
+              {   
+                var proc = true; 
+                if(data.recordset)
+                {
+                  //console.log(data.recordset);
+                  data.recordset.forEach((item)=>
+                  {  
+                         if(item.file_name && item.file_size && item.gallery == "NO" && proc)
+                         {
+                            proc = false ;
+                            
+                            img.src = `${d_config.url}get-file?session='${encodeURIComponent(session)}'&tableName=Categorys&idx=${encodeURI(item.idx)}`;
+                         }				   
+                    });
+                }
+                  
+                if(proc){
+                  const icon = document.createElement("i");
+                    icon.className = "fas fa-image";
+                    icon.title = "No image available";
+                    img.insertAdjacentElement("afterend", icon);
+                    img.style.display = "none";
+           
+                }
+             })
+             .catch(error => console.error('Error:', error));
+              
+             img.alt = item.category_name;
+
+             document.querySelector('.categories-grid.final').appendChild(category);
+         });
+    }
+})
+.catch((error) => {
+    console.error(error);
+});
    
 }
