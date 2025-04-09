0d790f244bd1d1ce26ab121f90b836b49aed314f
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-09 16:23:40 +0200

    Update script.js

--- assets/js/database/script.js
+++ assets/js/database/script.js
@@ -268,7 +268,7 @@ setTimeout(function()
 		    table.columns.filter((column) => column.form == "editor").forEach((column)=>
 		    {
 			var quillInstance = Quill.find(document.querySelector(`#add-item-form #${column.name}`));
-			var content = quillInstance.getText().root.innerHTML;   
+			var content = quillInstance.root.innerHTML;   
 			console.log(content);
 			formData.append(column.name, content); 
 		    });
