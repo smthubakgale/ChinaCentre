6835ed21d5dcc432c3b9d060b85ed720c8d9ef65
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-10 10:20:06 +0200

    Update script.js

--- assets/js/products/script.js
+++ assets/js/products/script.js
@@ -8,6 +8,129 @@ const urs = "https://example.com" + ( (qrs) ? '?' + qrs : '');
 let Params = getQueryParams(urs);  
 console.log(Params);
 
+loadroducts();
+
+function loadProducts(){
+  let bid = Params['brand'];
+  let cid = Params['category'];
+
+  let query = '';
+  if(cid){
+    query = `
+     SELECT 
+    p.idx,
+    p.product_name,
+    p.item_no,
+    p.main_dimension,
+    p.main_feature,
+    p.main_material,
+    p.price,
+    p.barcode,
+    p.quantity,
+    c.category_name,
+    b.brand_name,
+    p.availability
+  FROM 
+    Products p
+    INNER JOIN Categories c ON p.category_no = c.idx
+    INNER JOIN Brands b ON p.brand_no = b.idx
+  WHERE 
+    p.category_no = ${category_idx};
+    `;
+  }
+  
+  if(bid){
+    query = `
+      SELECT 
+    p.idx,
+    p.product_name,
+    p.item_no,
+    p.main_dimension,
+    p.main_feature,
+    p.main_material,
+    p.price,
+    p.barcode,
+    p.quantity,
+    c.category_name,
+    b.brand_name,
+    p.availability
+  FROM 
+    Products p
+    INNER JOIN Categories c ON p.category_no = c.idx
+    INNER JOIN Brands b ON p.brand_no = b.idx
+  WHERE 
+    p.brand_no = ${bid};
+    `;
+  }
+
+  if(query != '')
+  {
+      fetch(d_config.url + `database/query/exec?session=${encodeURIComponent(session)}&query=${btoa(query)}`)
+      .then((response) => response.json())
+      .then((data) => { 
+           console.log(data);
+          if(data.success && data.results)
+          {
+             document.querySelector(".products-container.final").innerHTML = "";
+             
+             data.results.recordset.forEach((item)=>{
+                 let product =  new DOMParser().parseFromString( `
+                      <div class="product-item">
+                        <img class="nav-link" href="#product" queries="${'product=' + item.idx}" src="" alt="">
+                        <h5>${item.product_name}</h5>
+                        <p><span class="old-price">R 999.99</span> R 699.99</p>
+                        <button class="carts"><i class="fas fa-shopping-cart"></i> Add to Cart</button>
+                      </div>
+                   `,  "text/html").body.firstChild;
+
+                  const carts = product.querySelector(".carts");
+            
+                  carts.onclick = ()=>{
+                      cart_add(item.idx , 1);
+                   };
+                  const img = product.querySelector("img");
+         
+                  fetch(d_config.url + `list-files?session='${encodeURIComponent(session)}'&tableName=Products&tableIdx=${item.idx}`)
+                 .then(response => response.json())
+                 .then((data) => 
+                  {   
+                    var proc = true; 
+                    if(data.recordset)
+                    {
+                      //console.log(data.recordset);
+                      data.recordset.forEach((item)=>
+                      {  
+                             if(item.file_name && item.file_size && item.gallery == "NO" && proc)
+                             {
+                                proc = false ;
+                                
+                                img.src = `${d_config.url}get-file?session='${encodeURIComponent(session)}'&tableName=Departments&idx=${encodeURI(item.idx)}`;
+                             }				   
+                        });
+                    }
+                      
+                    if(proc){
+                      const icon = document.createElement("i");
+                        icon.className = "fas fa-image";
+                        icon.title = "No image available";
+                        img.insertAdjacentElement("afterend", icon);
+                        img.style.display = "none";
+               
+                    }
+                 })
+                 .catch(error => console.error('Error:', error));
+                  
+                 img.alt = item.department_name;
+               
+                   document.querySelector(".products-container.final").appendChild(roduct);
+             });
+          }
+      })
+      .catch((error) => {
+          console.error(error);
+      }); 
+  } 
+}
 
 const filterBtn = document.querySelector('.filter-btn');
 const filterAside = document.querySelector('.filter-aside');
