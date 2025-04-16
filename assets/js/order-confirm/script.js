09d9c24819aedfe73c14b021792dc03a6846fcf5
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-16 14:28:30 +0200

    Update script.js

--- assets/js/order-confirm/script.js
+++ assets/js/order-confirm/script.js
@@ -13,7 +13,12 @@ fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)
     console.log(data); 
     if(data.success && data.results)
     {
+       if(data.results.recordset.length > 0){
+          var res = data.results.recordset[0];
 
+          document.querySelector(".idx").innerHTML = btoa(res.idx);
+          document.querySelector(".idx").style.opacity = 1;
+       }
     }
 })
 .catch((error) => {
