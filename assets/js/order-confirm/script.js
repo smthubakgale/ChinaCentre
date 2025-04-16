5e7707cc556e3fdaa5a149af8ecd332803927443
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-16 14:12:45 +0200

    Update script.js

--- assets/js/order-confirm/script.js
+++ assets/js/order-confirm/script.js
@@ -4,3 +4,18 @@ let query = `
   FROM User_Payments
   ORDER BY checkout_date DESC
 `;
+
+fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query)}`)
+.then((response) => { 
+    return response.json();
+})
+.then((data) => {
+    console.log(data); 
+    if(data.success && data.results)
+    {
+
+    }
+})
+.catch((error) => {
+    console.error(error);
+}); 
