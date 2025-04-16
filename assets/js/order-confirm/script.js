bafab3bf781af8c3f3a5947732a111faa6f5dd82
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-16 14:19:20 +0200

    Update script.js

--- assets/js/order-confirm/script.js
+++ assets/js/order-confirm/script.js
@@ -2,7 +2,7 @@
 let query = `
   SELECT TOP 1 *
   FROM User_Payments
-  ORDER BY checkout_date DESC
+  ORDER BY CONVERT(DATETIME, checkout_date, 126) DESC
 `;
 
 fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query)}`)
