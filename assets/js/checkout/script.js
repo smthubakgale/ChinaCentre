a1d65f14c52e2bb6a31fb25d2bfba2ac872cd31a
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-16 09:55:22 +0200

    Update script.js

--- assets/js/checkout/script.js
+++ assets/js/checkout/script.js
@@ -388,12 +388,13 @@ function loadShipping(){
 	      document.querySelector(".address-select select").style.opacity = 1;
 	      document.querySelector(".delete-button").style.opacity = 1;
 	      document.querySelector(".save-button-container").style.display = "block"; 
-		 
+		  = 
 	      address_action = "extra_address";
 	      document.getElementById('shipping-address').checked = false;
 	      document.getElementById('shipping-address').disabled = false;
 		 
 	      let res = data.results.recordset[0]; 
+	      has_addr = true;
 
 	      document.querySelector(".address-details .addr").innerHTML = `
                 <p>
