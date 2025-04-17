11cde6cee1bc7f47ffba8c24c50bec8bd7f01ec2
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2025-04-17 09:54:17 +0200

    Update script.js

--- assets/js/order-history/script.js
+++ assets/js/order-history/script.js
@@ -18,6 +18,105 @@ if (query['track-order'] === 'true') {
 }
 
 // Populate order history grid
+loadOrderHistory();
+function loadOrderHistory(){
+let query = `
+      SELECT *
+      FROM User_Payments
+      ORDER BY CONVERT(DATETIME, checkout_date + ':00', 126) DESC
+    `;
+    
+    fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query)}`)
+    .then((response) => { 
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data); 
+        if(data.success && data.results)
+        {
+            document.querySelector(".order-history-grid").innerHTML = '';
+            
+            data.results.recordset.forEach((res)=>
+            {
+                
+             let query2 = `
+                SELECT pc.*, p.product_name, p.price , p.product_name , COALESCE(c.category_name, '') AS category_name
+                FROM Product_Cart pc
+                JOIN Products p ON pc.product_no = p.idx
+                LEFT JOIN Categories c ON p.category_no = c.idx
+                WHERE pc.checkout_key = '${res.checkout_key}';
+             `;
+                
+               fetch(d_config.url + `database/query/exec?session='${encodeURIComponent(session)}'&query=${btoa(query2)}`)
+              .then((response) => { 
+                  return response.json();
+              })
+              .then((data) => {
+                  console.log(data); 
+                  if(data.success && data.results)
+                  {
+                       data.results.recordset.forEach((item)=>
+                       {
+                           let product = new DOMParser().parseFromString(`
+                               <div class="order-history-item">
+                               <img src="" alt="${item.product_name}" class="item nav-link" href="#products" queries="${'brand=' + item.product_no}" >
+                                <div class="order-history-item-info">
+                                    <h2>${item.product_name}</h2>
+                                    <p>Price: $${item.price}</p>
+                                    <p>Status: ${res.checkout_status}</p> 
+                                </div>
+                               </div>
+                               `, 
+                               "text/html").body.firstChild;
+    
+                                 const img = product.querySelector("img");
+                   
+                                  fetch(d_config.url + `list-files?session='${encodeURIComponent(session)}'&tableName=Products&tableIdx=${item.product_no}`)
+                                 .then(response => response.json())
+                                 .then((data) => 
+                                  {   
+                                    var proc = true; 
+                                    if(data.recordset)
+                                    {
+                                      console.log(data.recordset);
+                                      data.recordset.forEach((item)=>
+                                      {  
+                                             if(item.file_name && item.file_size && item.gallery == "NO" && proc)
+                                             {
+                                                proc = false ;
+                                                
+                                                img.src = `${d_config.url}get-file?session='${encodeURIComponent(session)}'&tableName=Brands&idx=${encodeURI(item.idx)}`;
+                                             }				   
+                                        });
+                                    }
+                                      
+                                    if(proc){
+                                      const icon = document.createElement("i");
+                                        icon.className = "fas fa-image";
+                                        icon.title = "No image available";
+                                        img.insertAdjacentElement("afterend", icon);
+                                        img.style.display = "none";
+                               
+                                    }
+                                 })
+                                 .catch(error => console.error('Error:', error));
+                           
+                                 document.querySelector(".order-history-grid").appendChild(product);
+                       });
+                  }
+              })
+              .catch((error) => {
+                  console.error(error);
+              }); 
+                
+            });
+        }
+    }) 
+    .catch((error) => {
+      console.error(error);
+    });    
+}
+/*
 const orderHistoryGrid = document.getElementById('order-history-grid');
 orderHistoryData.forEach((order) => {
     const item = document.createElement('div');
@@ -33,6 +132,7 @@ orderHistoryData.forEach((order) => {
     `;
     orderHistoryGrid.appendChild(item);
 });
+*/
 
 // Add event listener to status filter
 const statusFilter = document.getElementById('status-filter');
